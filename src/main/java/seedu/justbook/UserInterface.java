f6a41cb7b4c90927fa54b2da37cbff8c21b6f9b2
hana-oxy <44062805+hana-oxy@users.noreply.github.com>
2021-10-23 15:32:04 +0800

    Update UserInterface.java

--- src/main/java/seedu/justbook/UserInterface.java
+++ src/main/java/seedu/justbook/UserInterface.java
@@ -31,13 +31,12 @@ import static java.util.Comparator.comparing;
 
 public class UserInterface {
 
-    static final String logo = " ____        _        \n"
-            + "|  _ \\ _   _| | _____ \n"
-            + "| | | | | | | |/ / _ \\\n"
-            + "| |_| | |_| |   <  __/\n"
-            + "|____/ \\__,_|_|\\_\\___|\n";
-
     static void printGreeting() {
+        static final String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("What is your name?");
     }
